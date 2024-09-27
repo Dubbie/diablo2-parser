0d4b1d9e06b1218c711e00bc2ee4190e3ec8b96c
Miho Daniel <miho.dnl@gmail.com>
2024-09-27 14:26:17 +0200

    clean up dmg norm property handler

--- app/Handlers/PropertyHandlers/DmgNormPropertyHandler.php
+++ app/Handlers/PropertyHandlers/DmgNormPropertyHandler.php
@@ -19,37 +19,66 @@ class DmgNormPropertyHandler implements PropertyHandlerInterface
 
         foreach (self::PROPERTIES as $propertyName) {
             $isMin = strpos($propertyName, 'min') !== false;
-
-            $mappedProperty = new MappedProperty();
-            $mappedProperty->setProperty(Property::where('name', $propertyName)->first());
-
-            if ($isMin) {
-                $mappedProperty->setMin($itemProperty->min);
-                $mappedProperty->setMax($itemProperty->min);
-            } else {
-                $mappedProperty->setMin($itemProperty->max);
-                $mappedProperty->setMax($itemProperty->max);
-            }
-
-            $stats = [];
-            foreach ($itemProperty->baseProperty->propertyStats as $propertyStat) {
-                if ($isMin && strpos($propertyStat->stat, 'min') !== false) {
-                    $mappedStat = new MappedStat();
-                    $mappedStat->setStat($propertyStat->baseStat);
-                    $stats[] = $mappedStat;
-                }
-
-                if (!$isMin && strpos($propertyStat->stat, 'max') !== false) {
-                    $mappedStat = new MappedStat();
-                    $mappedStat->setStat($propertyStat->baseStat);
-                    $stats[] = $mappedStat;
-                }
-            }
-
-            $mappedProperty->setStats($stats);
+            $mappedProperty = $this->createMappedProperty($itemProperty, $propertyName, $isMin);
             $result[] = $mappedProperty;
         }
 
         return $result;
+    }
+
+    /**
+     * Creates a MappedProperty with the correct min/max values and associated stats.
+     *
+     * @param ItemProperty $itemProperty
+     * @param string $propertyName
+     * @param bool $isMin
+     * @return MappedProperty
+     */
+    private function createMappedProperty(ItemProperty $itemProperty, string $propertyName, bool $isMin): MappedProperty
+    {
+        $mappedProperty = new MappedProperty();
+        $property = Property::where('name', $propertyName)->first();
+
+        // Set the property details
+        $mappedProperty->setProperty($property);
+
+        // Assign min/max based on the flag
+        if ($isMin) {
+            $mappedProperty->setMin($itemProperty->min);
+            $mappedProperty->setMax($itemProperty->min);
+        } else {
+            $mappedProperty->setMin($itemProperty->max);
+            $mappedProperty->setMax($itemProperty->max);
+        }
+
+        // Set the associated stats
+        $mappedProperty->setStats($this->getMappedStats($itemProperty, $isMin));
+
+        return $mappedProperty;
+    }
+
+    /**
+     * Retrieves the MappedStat array for the given itemProperty.
+     *
+     * @param ItemProperty $itemProperty
+     * @param bool $isMin
+     * @return MappedStat[]
+     */
+    private function getMappedStats(ItemProperty $itemProperty, bool $isMin): array
+    {
+        $stats = [];
+
+        foreach ($itemProperty->baseProperty->propertyStats as $propertyStat) {
+            $isStatMin = strpos($propertyStat->stat, 'min') !== false;
+            $isStatMax = strpos($propertyStat->stat, 'max') !== false;
+
+            if (($isMin && $isStatMin) || (!$isMin && $isStatMax)) {
+                $mappedStat = new MappedStat();
+                $mappedStat->setStat($propertyStat->baseStat);
+                $stats[] = $mappedStat;
+            }
+        }
+
+        return $stats;
     }
 }

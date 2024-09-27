e99c446c2ddd5152ef7536dac06652a37c01d4b9
Miho Daniel <miho.dnl@gmail.com>
2024-09-27 15:00:38 +0200

    fix poison dmg description handler

--- app/Handlers/CustomDescriptionHandlers/DmgPoisonDescriptionHandler.php
+++ app/Handlers/CustomDescriptionHandlers/DmgPoisonDescriptionHandler.php
@@ -13,7 +13,7 @@ class DmgPoisonDescriptionHandler implements CustomDescriptionHandlerInterface
     {
         $min = $modifier->getRange('minValue')['min'];
         $max = $modifier->getRange('minValue')['max'];
-        $length = $modifier->getValues()[0];
+        $length = $modifier->getValues()['value'];
 
         $dmg = $this->calculatePoisonDamage($min, $max, $length);
         $formattedValue = StatFormatter::formatValue($dmg['min'], $dmg['max'], '');
